9b231f4eb029f9a4bbcd1d9b40bfc578fd8194f5
bradmartin <bradwaynemartin@gmail.com>
2018-08-21 01:12:47 -0500

    minor change

--- flexing/src/main/java/net/bradmartin/flexing/images.kt
+++ flexing/src/main/java/net/bradmartin/flexing/images.kt
@@ -1,2 +1,13 @@
 package net.bradmartin.flexing
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.widget.ImageView
+
+fun getBitmapFromImageView(value: ImageView): Bitmap {
+    // if not instance of ImageView throw an exception
+    if (value !is ImageView) throw IllegalArgumentException("Value passed to getBitmapFromImageView is not of type ImageView")
+
+    val drawable = value.drawable as BitmapDrawable
+    return drawable.bitmap
+}
