57ad3788a49efde967f069738e256ab117a8e345
bradmartin <bradwaynemartin@gmail.com>
2018-08-21 01:08:18 -0500

    make function static

--- flexing/src/main/java/net/bradmartin/flexing/Images.kt
+++ flexing/src/main/java/net/bradmartin/flexing/Images.kt
@@ -4,14 +4,12 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.widget.ImageView
 
-class Images {
-    fun getBitmapFromImageView(value: ImageView): Bitmap {
-        // if not instance of imageview throw an exception
-        if (value !is ImageView) {
-            throw IllegalArgumentException("Value passed to getBitmapFromImageView is not of type ImageView")
-        }
-
-        val drawable = value.drawable as BitmapDrawable
-        return drawable.bitmap
+fun getBitmapFromImageView(value: ImageView): Bitmap {
+    // if not instance of ImageView throw an exception
+    if (value !is ImageView) {
+        throw IllegalArgumentException("Value passed to getBitmapFromImageView is not of type ImageView")
     }
+
+    val drawable = value.drawable as BitmapDrawable
+    return drawable.bitmap
 }
